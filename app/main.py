3342c0a0bdade96a4391e3d46a967b76ff1e0e19
Isaiah-Mwinga <motongori@marketforce360.com>
2023-06-08 10:10:47 +0300

    users

--- app/main.py
+++ app/main.py
@@ -1,27 +1,37 @@
-import os   
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from config.database import engine
+from config.database import Base
+from auth import authrouter
+from users import usersrouter
 
-from app.users import auth
-
-from fastapi.testclient import TestClient
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
 
-app.include_router(auth.router)
+origins = ["*"]
 
-
-client = TestClient(app)
-
-# NEW
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/")
+def hello():
+    return "Hello"
+
+
+app.include_router(authrouter.router)
+app.include_router(usersrouter.router)
+
+
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("main:app", reload=True)
