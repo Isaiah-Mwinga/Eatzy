e95fb70e9f7f78e342c1e0a1e31d96dca5ec9341
Isaiah-Mwinga <motongori@marketforce360.com>
2023-09-02 21:58:30 +0300

    Eatzy

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -1,8 +1,13 @@
+
 import React from 'react'
 
 const Categories = () => {
   return (
-    <div>Categories</div>
+      <div className="flex justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-xl font-semibold">Categories</h1>
+        </div>
+      </div>
   )
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,23 +2,11 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="h-12 text-black p-4 flex items-center justify-between  uppercase md:h-24 lg:px-20 xl:px-40">
+    <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-white text-xl font-semibold">Eatzy</div>
-        <ul className="flex space-x-4 justify-center text-center">
-          <li>
-            <a href="/" className="text-white hover:text-gray-400">Home</a>
-          </li>
-          <li>
-            <a href="/categories" className="text-white hover:text-gray-400">Categories</a>
-          </li>
-          <li>
-            <a href="/cart" className="text-white hover:text-gray-400">Cart</a>
-          </li>
-          <li>
-            <a href="/profile" className="text-white hover:text-gray-400">Profile</a>
-          </li>
-        </ul>
+        <div className="space-x-4">
+        </div>
       </div>
     </nav>
   );
