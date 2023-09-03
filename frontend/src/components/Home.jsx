961a0ff7ae64e79f8d5b55fa82277e30ac61ecab
Isaiah-Mwinga <motongori@marketforce360.com>
2023-09-03 23:03:01 +0300

    Eatzy

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar'; // Import your Navbar component
-import SearchBar from './SearchBar'; // Import your SearchBar component
 import RestaurantCard from './RestaurantCard'; // You can create a component for displaying restaurant cards
 import Footer from './Footer'; // Import your Footer component
 import Categories from './Categories';
@@ -11,7 +10,6 @@ const Home = () => {
   return (
     <div>
       <Navbar />
-      <SearchBar />
       <Categories />
       <RestaurantCard />
       <Footer />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import { Icon } from '@iconify/react';
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="text-white text-xl font-semibold">Eatzy</div>
-        <div className="space-x-4">
+    <nav className="bg-purple-50 py-4 px-8">
+      <SearchBar />
+      <div className='flex justify-between items-center'>
+        <div className='flex items-center'>
+          <h1 className='text-2xl font-semibold ml-2'>Eatzy</h1>
         </div>
+        <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-2 rounded-md w-full">
+        <Icon icon="ph:phone" />
+          <span>+254 745 10 1544</span>
+        </div>
+        <div className="relative">
+        <Icon icon="bxs:cart" className="text-3xl"/>
+        <p className="bg-orange-300 absolute -top-0 -right-0 text-[12px] rounded-full text-white h-4 w-4 grid place-items-center">3</p>
+        </div>
+      </div>
       </div>
     </nav>
   );
