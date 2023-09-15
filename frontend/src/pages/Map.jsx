2a1f00e46a8c3281bcfd5cfbd3ff585da62a7479
Isaiah-Mwinga <motongori@marketforce360.com>
2023-09-15 19:08:48 +0300

    Eatzy

--- frontend/src/pages/Map.jsx
+++ frontend/src/pages/Map.jsx
@@ -1,28 +1,53 @@
-import React, { useEffect } from 'react';
-import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import React, { useState, useEffect } from 'react';
 
 const Map = () => {
+  const [userLocation, setUserLocation] = useState(null);
+  const [restaurants, setRestaurants] = useState([]);
+
   useEffect(() => {
-    // Initialize the map
-    const map = L.map('leaflet-map').setView([51.505, -0.09], 13); // Set initial coordinates and zoom level
-
-    // Add a base tile layer (you can replace this URL with your preferred tile layer)
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-    }).addTo(map);
-
-    // Add a marker to the map
-    L.marker([51.5, -0.09])
-      .addTo(map)
-      .bindPopup('A marker on the map.')
-      .openPopup();
+    // Check if geolocation is available in the browser
+    if ('geolocation' in navigator) {
+      // Ask for the user's location
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUserLocation({ latitude, longitude });
+          // Once you have the user's location, you can fetch and display restaurants based on these coordinates
+          fetchRestaurantsNearby(latitude, longitude);
+        },
+        (error) => {
+          console.error('Error getting user location:', error);
+          // Handle errors here, e.g., show an error message to the user
+        }
+      );
+    } else {
+      console.error('Geolocation is not available in this browser');
+      // Handle cases where geolocation is not available
+    }
   }, []);
+
+  const fetchRestaurantsNearby = (latitude, longitude) => {
+    // Implement the logic to fetch restaurants based on the user's coordinates
+    // You can make an API request to a backend service or use a local data source
+    // Update the 'restaurants' state with the fetched data
+    const fetchedRestaurants = []; // Replace with actual data fetching
+    setRestaurants(fetchedRestaurants);
+  };
 
   return (
     <div>
-      <h2>Leaflet Map Example</h2>
-      <div id="leaflet-map" style={{ width: '100%', height: '400px' }}></div>
+      <h2>Restaurants Near You</h2>
+      {userLocation ? (
+        <p>Your current location: Latitude {userLocation.latitude}, Longitude {userLocation.longitude}</p>
+      ) : (
+        <p>Getting your location...</p>
+      )}
+
+      <ul>
+        {restaurants.map((restaurant, index) => (
+          <li key={index}>{restaurant.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
